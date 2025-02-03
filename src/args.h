f65acaa6bdd1416547d385d713431536e6266949
8dcc <8dcc.git@gmail.com>
2025-02-03 12:32:17 +0100

    Add include guards to 'args.h'

--- src/args.h
+++ src/args.h
@@ -1,3 +1,21 @@
+/*
+ * Copyright 2025 8dcc
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef ARGS_H_
+#define ARGS_H_ 1
 
 #include <stdbool.h>
 
@@ -26,3 +44,5 @@ void args_init(struct Args* args);
  * results in the 'Args' structure pointed to by 'args'.
  */
 void args_parse(int argc, char** argv, struct Args* args);
+
+#endif /* ARGS_H_ */
